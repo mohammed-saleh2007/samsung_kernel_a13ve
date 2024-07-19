5110c5fa507725ad2e7f4c745493f12ef843dc21
Mohammed Saleh <140978153+mohammed-saleh2007@users.noreply.github.com>
2024-07-19 17:21:42 +0000

    update

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,15 +7,15 @@ export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 
 export ARCH=arm64
-export ANDROID_MAJOR_VERSION=u
+#export ANDROID_MAJOR_VERSION=u
 
-export KBUILD_BUILD_USER=sudoWTF
-export KBUILD_BUILD_HOST=archlinux
+#export KBUILD_BUILD_USER=sudoWTF
+#export KBUILD_BUILD_HOST=archlinux
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a13ve_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j8
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
